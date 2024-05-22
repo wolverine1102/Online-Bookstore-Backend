01f3456c0128e1d6d87785a9e6eccc4887f4f192
Shreyosi Sarkar <21f1003968@ds.study.iitm.ac.in>
2024-05-22 18:47:28 +0530

    Add src/controllers/bookController/updateBook

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -2,23 +2,41 @@ const _ = require('lodash');
 const Book = require('../models/book');
 
 const getAllBooks = async function (req, res) {
-    const books = await Book.find({}).select('-_id -__v');
+    try {
+        const books = await Book.find({}).select('-_id -__v');
 
-    res.status(200).send(books);
-    return;
-};
-
-const getBook = async function (req, res) {
-    const book = await Book.findOne({ id: req.params.id }).select('-_id -__v')
-
-    if (!book) {
-        res.status(404).json({
-            message: 'Book not found'
+        res.status(200).send(books);
+        return;
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: 'Internal Server Error'
         });
         return;
     }
-    else {
-        res.status(200).send(book);
+};
+
+const getBook = async function (req, res) {
+    try {
+        const book = await Book.findOne({ id: req.params.id }).select('-_id -__v')
+
+        if (!book) {
+            res.status(404).json({
+                message: 'Book not found'
+            });
+            return;
+        }
+        else {
+            res.status(200).send(book);
+            return;
+        }
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: 'Internal Server Error'
+        });
         return;
     }
 };
@@ -88,7 +106,55 @@ const createBook = async function (req, res) {
 };
 
 const updateBook = async function (req, res) {
+    try {
+        let errors = {};
+        let updates = {};
 
+        ['title', 'author'].forEach(key => {
+            if (req.body[key] === "") {
+                errors[key] = `${key} cannot be empty`;
+            }
+        });
+
+        if (_.keys(errors).length > 0) {
+            res.status(400).json(errors);
+            return;
+        };
+
+        ['title', 'author', 'ISBN', 'pages', 'publisher', 'languages'].forEach(key => {
+            if (req.body[key]) {
+                updates[key] = req.body[key];
+            }
+        });
+
+        await Book.findOneAndUpdate(
+            { id: req.params.id },
+            updates,
+            { new: true }
+        ).select('-_id -__v')
+            .then((updatedBook) => {
+                if (updatedBook) {
+                    res.status(200).send(updatedBook);
+                    return;
+                }
+                else {
+                    res.status(401).json({
+                        message: 'This book does not exists.'
+                    });
+                    return;
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: 'Internal Server Error'
+        });
+        return;
+    }
 };
 
 const deleteBook = async function (req, res) {
